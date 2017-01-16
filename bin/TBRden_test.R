4cf2b9fd256549fd53204bddeecbb9564db414c6
RCollins13 <Ryan Collins>
2017-01-15 23:35:41 -0500

    Debug

--- bin/TBRden_test.R
+++ bin/TBRden_test.R
@@ -20,4 +20,5 @@ args <- commandArgs(trailingOnly=T)
 print(args)
 
 #Run TBRden
-TBRden(args[1],args[2],OUTDIR=args[3],prefix=args[4],adjusted=args[5],manColor=args[6])
+TBRden(args[1],args[2],OUTDIR=args[3],prefix=args[4],
+	adjusted=as.numeric(args[5]),manColor=args[6])
