16888b133206d736baebd03ef427c30288debe8c
RCollins13 <rlcollins@g.harvard.edu>
2017-03-14 21:56:04 -0400

    Add simulate_intervals.sh

---
+++ bin/simulate_intervals.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+##############################
+#    Rare CNV Map Project    #
+##############################
+
+# Copyright (c) 2017 Ryan L. Collins
+# Distributed under terms of the MIT License (see LICENSE)
+# Contact: Ryan L. Collins <rlcollins@g.harvard.edu>
+# Code development credits availble on GitHub
+
+# Permutation test of CNV burdens by local CNV shifting
+
+#Usage statement
+usage(){
+cat <<EOF
+usage: simulate_intervals.sh [-h] [-s SIZE] [-d STDEV] [-N COUNT] [-x EXCLUDE] [-o OUTFILE] [-z] GENOME
+
+Simulates BED-style intervals whose sizes are normally distributed
+
+Positional arguments:
+  GENOME     BEDTools-style genome file (tab delimmed two-column file: chr, length)
+
+Optional arguments:
+  -h  HELP          Show this help message and exit
+  -s  SIZE          Average size of simulated intervals (default: 5000bp)
+  -d  STDEV         Standard deviation of simulated interval sizes (default: 1000bp)
+  -N  COUNT         Number of intervals to simulate (default: 10000)
+  -x  EXCLUDE       BED-style intervals to exclude when simulating intervals
+  -o  OUTFILE       Output file (default: stdout)
+  -z  GZIP          Gzip output
+EOF
+}
+
+#Parse arguments
+SIZE=5000
+STDEV=1000
+COUNT=10000
+EXCLUDE=0
+OUTFILE=/dev/stdout
+GZ=0
+while getopts ":s:d:N:x:o:zh" opt; do
+  case "$opt" in
+    h)
+      usage
+      exit 0
+      ;;
+    s)
+      SIZE=${OPTARG}
+      ;;
+    d)
+      STDEV=${OPTARG}
+      ;;
+    N)
+      COUNT=${OPTARG}
+      ;;
+    x)
+      EXCLUDE=${OPTARG}
+      ;;
+    o)
+      OUTFILE=${OPTARG}
+      ;;
+    z)
+      GZ=1
+      ;;
+  esac
+done
+shift $((${OPTIND} - 1))
+GENOME=$1
+
+#Check that genome file exists
+if ! [ -s ${GENOME} ]; then
+  echo -e "\nERROR: GENOME FILE DOES NOT EXIST\n"
+  usage
+  exit 0
+fi
+
+#Subtract exclusion intervals from genome file if specified
+SAMPSPACE=`mktemp`
+if [ ${EXCLUDE} != 0 ]; then
+  if ! [ -s ${EXCLUDE} ]; then
+    echo -e "\nERROR: -x IS SPECIFIED BUT EXCLUSION FILE DOES NOT EXIST\n"
+    usage
+    exit 0
+  else
+    bedtools subtract -a <( awk -v OFS="\t" '{ print $1, "1", $2 }' ${GENOME} ) \
+    -b ${EXCLUDE} > ${SAMPSPACE}
+  fi
+else
+  awk -v OFS="\t" '{ print $1, "1", $2 }' ${GENOME} > ${SAMPSPACE}
+fi
+NGENOME=`mktemp`
+
+#Compute length modifiers for intervals (5x total count requested)
+MODS=`mktemp`
+Rscript -e "write.table(round(rnorm(n=5*${COUNT},sd=${STDEV})),\"${MODS}\",\
+  col.names=F,row.names=F,sep=\"\t\",quote=F)"
+
+#Generate random intervals
+bedtools random -l ${SIZE} -n $((5*${COUNT})) -g ${GENOME} | paste - ${MODS} | \
+awk -v OFS="\t" '{ print $1, $2+$NF, $3 }' | awk -v OFS="\t" \
+'{ if ($3-$2>=1 && $2>0) print $0 }' | bedtools intersect -f 1 -a - -b ${SAMPSPACE} | \
+head -n ${COUNT} | sort -Vk1,1 -k2,2n -k3,3n > ${OUTFILE}
+
+#Gzip output if optioned
+if [ ${OUTFILE} != "/dev/stdout" ] && [ ${GZ} -gt 0 ]; then
+  gzip -f ${OUTFILE}
+fi
+
+#Clean up
+rm -rf ${SAMPSPACE} ${MODS}
--- working_code/curate_annotations.sh
+++ working_code/curate_annotations.sh
@@ -30,6 +30,26 @@ SFARI_ANNO=/data/talkowski/Samples/SFARI/ASC_analysis/annotations
 mkdir ${WRKDIR}/data/master_annotations
 mkdir ${WRKDIR}/data/master_annotations/genelists
 
+#####Gencode
+mkdir ${WRKDIR}/data/master_annotations/gencode
+#Download & unpack
+cd ${WRKDIR}/data/master_annotations/gencode
+wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz
+gunzip ${WRKDIR}/data/master_annotations/gencode/gencode.v19.annotation.gtf.gz
+#Cut list of all gene symbols
+fgrep -v "#" ${WRKDIR}/data/master_annotations/gencode/gencode.v19.annotation.gtf | \
+awk '{ if ($3=="gene") print $0 }' | sed 's/\;/\n/g' | fgrep "gene_name" | tr -d "\"" | \
+awk '{ print $2 }' | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/Gencode_v19_all.genes.list
+#Cut list gene symbols for select transcript types
+for class in antisense lincRNA miRNA protein_coding pseudogene snoRNA snRNA; do
+  echo ${class}
+  fgrep -v "#" ${WRKDIR}/data/master_annotations/gencode/gencode.v19.annotation.gtf | \
+  awk '{ if ($3=="gene") print $0 }' | fgrep "transcript_type \"${class}\"" | \
+  sed 's/\;/\n/g' | fgrep "gene_name" | tr -d "\"" | awk '{ print $2 }' | sort | uniq > \
+  ${WRKDIR}/data/master_annotations/genelists/Gencode_v19_${class}.genes.list
+done
+
 #####GENE LISTS
 #GTEx tissue-specific expressed genes
 cp ${SFARI_ANNO}/genelists/GTEx*specific.genes.list \
@@ -95,17 +115,49 @@ while read tissue; do
   readlink -f ${WRKDIR}/data/master_annotations/genelists/GTEx_${tissue}_not_expressed.genes.list
 done < <( l ${SFARI_ANNO}/misc/GTEx_expression/*bed.gz | fgrep -v highExpressor | \
 sed 's/\//\t/g' | awk '{ print $NF }' | sed 's/\./\t/g' | cut -f2 ) | paste - - - -
-
-
-
-
-
-
-
-
-
-
-
+#Constrained genes (pLI > 0.9)
+cd ${WRKDIR}/data/misc/
+wget ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/functional_gene_constraint/fordist_cleaned_nonpsych_z_pli_rec_null_data.txt
+awk '{ if ($20>0.9 && $20!="NA") print $2 }' \
+${WRKDIR}/data/misc/fordist_cleaned_nonpsych_z_pli_rec_null_data.txt | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/ExAC_constrained.genes.list
+#Dispensable genes (pLI < 0.1)
+awk '{ if ($20<0.1 && $20!="NA") print $2 }' \
+${WRKDIR}/data/misc/fordist_cleaned_nonpsych_z_pli_rec_null_data.txt | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/ExAC_dispensable.genes.list
+#Highly constrained genes (pLI > 0.9 & LOF obs:exp top half among constrained genes )
+nconst=$( sed '1d' ${WRKDIR}/data/misc/fordist_cleaned_nonpsych_z_pli_rec_null_data.txt | \
+awk '{ if ($20>0.9 && $20!="NA" && $16>0) print $0 }' | wc -l )
+sed '1d' ${WRKDIR}/data/misc/fordist_cleaned_nonpsych_z_pli_rec_null_data.txt | \
+awk -v OFS="\t" '{ if ($20>0.9 && $20!="NA" && $16>0) print $13/$16, $2 }' | \
+sort -nk1,1 | head -n $((${nconst}/2)) | cut -f2 | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/ExAC_highly_constrained.genes.list
+#Extremely constrained genes (pLI > 0.9 & no LoF mutations observed in ExAC)
+sed '1d' ${WRKDIR}/data/misc/fordist_cleaned_nonpsych_z_pli_rec_null_data.txt | \
+awk -v OFS="\t" '{ if ($20>0.9 && $20!="NA" && $16>0 && $13==0) print $2 }' | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/ExAC_extremely_constrained.genes.list
+#Intolerant genes (RVIS top 10% for MAF < 0.1% in any ExAC population)
+cd ${WRKDIR}/data/misc/
+wget http://genic-intolerance.org/data/RVIS_Unpublished_ExAC_May2015.txt
+sed '1d' ${WRKDIR}/data/misc/RVIS_Unpublished_ExAC_May2015.txt | \
+awk '{ if ($11<10) print $5 }' | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/RVIS_intolerant.genes.list
+#Highly intolerant genes (RVIS top 5% for MAF < 0.1% in any ExAC population)
+sed '1d' ${WRKDIR}/data/misc/RVIS_Unpublished_ExAC_May2015.txt | \
+awk '{ if ($11<5) print $5 }' | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/RVIS_highly_intolerant.genes.list
+#Extremely intolerant genes (RVIS top 1% for MAF < 0.1% in any ExAC population)
+sed '1d' ${WRKDIR}/data/misc/RVIS_Unpublished_ExAC_May2015.txt | \
+awk '{ if ($11<1) print $5 }' | sort | uniq > \
+${WRKDIR}/data/master_annotations/genelists/RVIS_extremely_intolerant.genes.list
+#OMIM disease-associated genes
+cd ${WRKDIR}/data/misc/
+git clone https://github.com/macarthur-lab/gene_lists.git
+mv gene_lists MacArthur_gene_lists
+sed '1d' ${WRKDIR}/data/misc/MacArthur_gene_lists/other_data/omim.full.tsv | \
+awk -v OFS="\n" '$4 !~ /NA/ { print $1, $3 }' | sed -e 's/|/\n/g' -e 's/,/\n/g' | \
+awk '{ if ($1!="NA") print $0 }' | sort | uniq | sed '/^$/d' > \
+${WRKDIR}/data/master_annotations/genelists/OMIM_all.genes.list
 
 
 
