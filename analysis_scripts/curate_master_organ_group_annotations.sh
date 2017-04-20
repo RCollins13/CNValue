8b95a74bb2251f231f36145e03de9fda738c81d3
Ryan L Collins <rlcollins@g.harvard.edu>
2017-04-20 12:35:39 -0400

    Reformat parameters file

--- analysis_scripts/curate_master_organ_group_annotations.sh
+++ analysis_scripts/curate_master_organ_group_annotations.sh
@@ -12,19 +12,8 @@
 #Code to curate organ-level master noncoding annotation tracks
 
 #####Set parameters
-WRKDIR=/data/talkowski/Samples/rCNVmap
-cd ${WRKDIR}
-h37=/data/talkowski/tools/ref/Ensembl_hgGRCh37_71_reord_bwa07/Ensembl_hgGRCh37_71_ERCC_reord.fa
-TMPDIR=/scratch/miket/rlc47temp/tmp.files/
-module load bedtools/2.22.1
-module load samtools/1.3
-module load hdf/1.8.14
-module load anaconda/4.0.5
-module rm gcc-4.4
-module load gcc/4.9.0
-module load bcftools/1.3.1
-PICARD=/data/talkowski/tools/bin/picard-tools-1.137/picard.jar
-SFARI_ANNO=/data/talkowski/Samples/SFARI/ASC_analysis/annotations
+export WRKDIR=/data/talkowski/Samples/rCNVmap
+source ${WRKDIR}/bin/rCNVmap/misc/rCNV_code_parameters.sh
 
 #####Read arguments
 tissue=$1
---
+++ misc/rCNV_code_parameters.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+##############################
+#    Rare CNV Map Project    #
+##############################
+
+# Copyright (c) 2017 Ryan L. Collins
+# Distributed under terms of the MIT License (see LICENSE)
+# Contact: Ryan L. Collins <rlcollins@g.harvard.edu>
+# Code development credits availble on GitHub
+
+#MASTER PARAMETERS FILE
+
+#Parameters
+export WRKDIR=/data/talkowski/Samples/rCNVmap
+cd ${WRKDIR}
+export h37=/data/talkowski/tools/ref/Ensembl_hgGRCh37_71_reord_bwa07/Ensembl_hgGRCh37_71_ERCC_reord.fa
+export TMPDIR=/scratch/miket/rlc47temp/tmp.files/
+module load bedtools/2.22.1
+module load samtools/1.3
+module load hdf/1.8.14
+module load anaconda/4.0.5
+module rm gcc-4.4
+module load gcc/4.9.0
+module load bcftools/1.3.1
+export SFARI_ANNO=/data/talkowski/Samples/SFARI/ASC_analysis/annotations
