3e43656a4d0149c8bafb2ed2b298310e7b604f6e
Ryan L Collins <rlcollins@g.harvard.edu>
2017-05-01 11:04:55 -0400

    Add sorted prelim annotation sets

--- working_code/run_burden_tests.sh
+++ working_code/run_burden_tests.sh
@@ -42,49 +42,98 @@ done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.li
           cut -f1 | fgrep -v CTRL )
 
 #####Run _preliminary_ annotation set burden testing
-#iterate & prep directories
 while read pheno; do
   for CNV in CNV DEL DUP; do
     for VF in E2 N1; do
       for filt in all noncoding; do
-    bsub -q normal -sla miket_sc -u nobody -J ${pheno}_${CNV}_${VF}_${filt}_annoSet_burdens \
-    "${WRKDIR}/bin/rCNVmap/bin/annoSet_burdenTest_batch.sh -N 1000 \
-      -x /data/talkowski/rlc47/src/GRCh37_Nmask.bed \
-      -p ${pheno}_${CNV}_${filt}_${VF} \
-      -o ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/ \
-      ${WRKDIR}/data/CNV/CNV_MASTER/CTRL/CTRL.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
-      ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
-      ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.prelim_subset.list \
-      ${WRKDIR}/data/misc/GRCh37_autosomes.genome"
+        bsub -q normal -sla miket_sc -u nobody -J ${pheno}_${CNV}_${VF}_${filt}_annoSet_burdens \
+        "${WRKDIR}/bin/rCNVmap/bin/annoSet_burdenTest_batch.sh -N 1000 \
+          -x /data/talkowski/rlc47/src/GRCh37_Nmask.bed \
+          -p ${pheno}_${CNV}_${filt}_${VF} \
+          -o ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/ \
+          ${WRKDIR}/data/CNV/CNV_MASTER/CTRL/CTRL.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
+          ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
+          ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.prelim_subset.list \
+          ${WRKDIR}/data/misc/GRCh37_autosomes.genome"
+      done
+    done
   done
 done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.list | \
           cut -f1 | fgrep -v CTRL )
 
-#####Run _full_annotation set burden testing
-#iterate & prep directories
+#####Collect _preliminary annotation set burden test results
+#Grouped by CNV type, VF, and CNV filter. MxN matrix; M: phenos, N: annotation sets
+#One matrix of p-values and one matrix of effect sizes per set of filters
+#Fold-changes
+for CNV in CNV DEL DUP; do
+  for VF in E2 N1; do
+    for filt in all noncoding; do
+      while read anno annopath; do
+        echo "${anno}"
+        while read pheno; do
+          if [ -e ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/${pheno}_${CNV}_${filt}_${VF}.${anno}.CNV_burden_results.txt ]; then
+            nfold=$( fgrep -v "#" \
+              ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/${pheno}_${CNV}_${filt}_${VF}.${anno}.CNV_burden_results.txt | \
+              awk -v OFS="\t" '{ print $11 }' )
+          else
+            nfold=NA
+          fi
+          if [ -z ${nfold} ]; then
+            nfold=NA
+          fi
+          echo ${nfold}
+        done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.list | \
+          cut -f1 | fgrep -v CTRL ) | paste -s
+      done < ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.prelim_subset.list | paste - -
+    done
+  done
+done
+
+#####Run _secondary_ annotation set burden testing
 while read pheno; do
   for CNV in CNV DEL DUP; do
-    #CODE:
-    # for filt in all coding haplosufficient noncoding intergenic; do
-    #   for VF in E2 E3 E4 N1; do
-    #     #Submit batch job
-    #     ${WRKDIR}/bin/rCNVmap/bin/annoSet_burdenTest_batch.sh -N 1000 \
-    #       -x /data/talkowski/rlc47/src/GRCh37_Nmask.bed \
-    #       -p ${pheno}_${CNV}_${filt}_${VF} \
-    #       -o ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/ \
-    #       ${WRKDIR}/data/CNV/CNV_MASTER/CTRL/CTRL.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
-    #       ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
-    #       ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.list \
-    #       ${WRKDIR}/data/misc/GRCh37_autosomes.genome
-    #   done
-    # done
-    #PARALLELIZE:
-    bsub -q normal -sla miket_sc -u nobody -J ${pheno}_${CNV}_annoSet_burdens \
-    "${WRKDIR}/bin/rCNVmap/analysis_scripts/parallelize_batched_annoSet_burdens.sh \
-    ${pheno} ${CNV}"
+    for VF in E2 N1; do
+      for filt in all noncoding; do
+        bsub -q normal -sla miket_sc -u nobody -J ${pheno}_${CNV}_${VF}_${filt}_annoSet_burdens_secondary \
+        "${WRKDIR}/bin/rCNVmap/bin/annoSet_burdenTest_batch.sh -N 1000 \
+          -x /data/talkowski/rlc47/src/GRCh37_Nmask.bed \
+          -p ${pheno}_${CNV}_${filt}_${VF} \
+          -o ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/ \
+          ${WRKDIR}/data/CNV/CNV_MASTER/CTRL/CTRL.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
+          ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
+          ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.secondary_subset.list \
+          ${WRKDIR}/data/misc/GRCh37_autosomes.genome"
+      done
+    done
   done
 done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.list | \
           cut -f1 | fgrep -v CTRL )
+
+# #####Run _full_ annotation set burden testing
+# #iterate & prep directories
+# while read pheno; do
+#   for CNV in CNV DEL DUP; do
+#     #CODE:
+#     # for filt in all coding haplosufficient noncoding intergenic; do
+#     #   for VF in E2 E3 E4 N1; do
+#     #     #Submit batch job
+#     #     ${WRKDIR}/bin/rCNVmap/bin/annoSet_burdenTest_batch.sh -N 1000 \
+#     #       -x /data/talkowski/rlc47/src/GRCh37_Nmask.bed \
+#     #       -p ${pheno}_${CNV}_${filt}_${VF} \
+#     #       -o ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/ \
+#     #       ${WRKDIR}/data/CNV/CNV_MASTER/CTRL/CTRL.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
+#     #       ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
+#     #       ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.list \
+#     #       ${WRKDIR}/data/misc/GRCh37_autosomes.genome
+#     #   done
+#     # done
+#     #PARALLELIZE:
+#     bsub -q normal -sla miket_sc -u nobody -J ${pheno}_${CNV}_annoSet_burdens \
+#     "${WRKDIR}/bin/rCNVmap/analysis_scripts/parallelize_batched_annoSet_burdens.sh \
+#     ${pheno} ${CNV}"
+#   done
+# done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.list | \
+#           cut -f1 | fgrep -v CTRL )
 
 
 
