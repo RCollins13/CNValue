631e1d64a8f49d2181dda350479f3eb0a1bf6bdc
Ryan L Collins <rlcollins@g.harvard.edu>
2017-06-26 13:37:13 -0400

    Add universe definitions to constraint decile gene list

--- working_code/run_burden_tests.sh
+++ working_code/run_burden_tests.sh
@@ -254,31 +254,6 @@ done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.li
 
 
 
-# #####Run _full_ annotation set burden testing
-# #iterate & prep directories
-# while read pheno; do
-#   for CNV in CNV DEL DUP; do
-#     #CODE:
-#     # for filt in all coding haplosufficient noncoding intergenic; do
-#     #   for VF in E2 E3 E4 N1; do
-#     #     #Submit batch job
-#     #     ${WRKDIR}/bin/rCNVmap/bin/annoSet_burdenTest_batch.sh -N 1000 \
-#     #       -x /data/talkowski/rlc47/src/GRCh37_Nmask.bed \
-#     #       -p ${pheno}_${CNV}_${filt}_${VF} \
-#     #       -o ${WRKDIR}/analysis/annoSet_burden/${pheno}/${CNV}/${filt}/${VF}/ \
-#     #       ${WRKDIR}/data/CNV/CNV_MASTER/CTRL/CTRL.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
-#     #       ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz \
-#     #       ${WRKDIR}/bin/rCNVmap/misc/master_noncoding_annotations.list \
-#     #       ${WRKDIR}/data/misc/GRCh37_autosomes.genome
-#     #   done
-#     # done
-#     #PARALLELIZE:
-#     bsub -q normal -sla miket_sc -u nobody -J ${pheno}_${CNV}_annoSet_burdens \
-#     "${WRKDIR}/bin/rCNVmap/analysis_scripts/parallelize_batched_annoSet_burdens.sh \
-#     ${pheno} ${CNV}"
-#   done
-# done < <( fgrep -v "#" ${WRKDIR}/bin/rCNVmap/misc/analysis_group_HPO_mappings.list | \
-#           cut -f1 | fgrep -v CTRL )
 
 
 
