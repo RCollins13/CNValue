3924ac29e65bfd0714c45edb5fa4c69298390e0c
Ryan L Collins <rlcollins@g.harvard.edu>
2017-08-25 23:07:35 -0400

    Create list of noncoding annotation classes to prioritize for annoScore

--- plotting_code/SMARCA2/gather_SMARCA2_fig_plot_data.sh
+++ plotting_code/SMARCA2/gather_SMARCA2_fig_plot_data.sh
@@ -25,7 +25,7 @@ mkdir ${WRKDIR}/data/plot_data/SMARCA2
 for pheno in NDD CTRL; do
   for CNV in CNV DEL DUP; do
     for VF in E2 E4; do
-      for filt in all haplosufficient; do
+      for filt in all coding haplosufficient; do
         zcat ${WRKDIR}/data/CNV/CNV_MASTER/${pheno}/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz | \
         fgrep -v "#"  | bedtools intersect -wa -a - -b <( echo -e "9\t1250000\t3250000" ) > \
         ${WRKDIR}/data/plot_data/SMARCA2/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed
@@ -40,7 +40,7 @@ step=100
 for pheno in NDD CTRL; do
   for CNV in CNV DEL DUP; do
     for VF in E2 E4; do
-      for filt in all haplosufficient; do
+      for filt in all coding haplosufficient; do
         paste <( seq 1250000 ${step} 3249900 ) <( seq 1250100 ${step} 3250000 ) | \
         awk -v OFS="\t" '{ print "9", $1, $2 }' | bedtools intersect -c -wa -a - \
         -b  ${WRKDIR}/data/plot_data/SMARCA2/${pheno}.${CNV}.${VF}.GRCh37.${filt}.bed.gz > \
